ef5e1fc4812c8b437f425f7547239394771139b0
Amir Maghami <Maghami.A1987@gmail.com>
2025-04-12 00:36:06 +0330

    fix docker

--- html/wp-content/themes/studiare/single-sc_megamenu.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * The template for displaying all footers
- */
-
-get_header();
-the_content();
-get_footer(); 
-
--- html/wp-content/themes/studiare/woocommerce/sc_templates/sc_post_excerpt.php
+++
@@ -1,8 +0,0 @@
-<?php
-		$mycontent_post = get_post(get_the_ID());
-		$my_excerpt = $mycontent_post->post_excerpt;
-		if( $my_excerpt != null){ ?>
-            <div class="product-single-content" id="sc-product-single-excerpt">
-               <?php echo $my_excerpt; ?>
-            </div>
-<?php } ?>
